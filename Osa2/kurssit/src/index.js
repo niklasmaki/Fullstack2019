1a200f9039069b6999e10e445e83d0d4ca75ac10
Niklas Mäki <niklas.maki@gmail.com>
2019-01-24 20:57:34 +0200

    Changed identation

--- Osa2/kurssit/src/index.js
+++ Osa2/kurssit/src/index.js
@@ -2,68 +2,68 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = props =>
-    <h1>{props.course}</h1>
+  <h1>{props.course}</h1>
 
-const Total = ({parts}) => {
-    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
 
-    return <p>yhteensä {total} tehtävää</p>
+  return <p>yhteensä {total} tehtävää</p>
 }
 
 
 const Part = props =>
-    <p>{props.part.name} {props.part.exercises}</p>
+  <p>{props.part.name} {props.part.exercises}</p>
 
 
 const Content = ({ parts }) => (
-    <div>
-        {parts.map((part) => <Part key={part.id} part={part} />)}
-    </div>
+  <div>
+    {parts.map((part) => <Part key={part.id} part={part} />)}
+  </div>
 )
 
 const Course = ({ course }) => (
-    <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total parts={course.parts} />
-    </div>
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
 )
 
 const App = () => {
-    const course = {
-        name: 'Half Stack -sovelluskehitys',
-        parts: [
-            {
-                name: 'Reactin perusteet',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Tiedonvälitys propseilla',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'Komponenttien tila',
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: 'Node.js perusteet',
-                exercises: 999,
-                id: 4
-            }
-        ]
-    }
+  const course = {
+    name: 'Half Stack -sovelluskehitys',
+    parts: [
+      {
+        name: 'Reactin perusteet',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Tiedonvälitys propseilla',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'Komponenttien tila',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Node.js perusteet',
+        exercises: 999,
+        id: 4
+      }
+    ]
+  }
 
-    return (
-        <div>
-            <Course course={course} />
-        </div>
-    )
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 )
