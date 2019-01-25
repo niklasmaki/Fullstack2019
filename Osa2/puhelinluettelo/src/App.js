2006885e358a5c70e669c1759a8f0f5240886c5e
Niklas Mäki <niklas.maki@gmail.com>
2019-01-25 15:53:04 +0200

    2.6

---
+++ Osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const updateNewName = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Puhelinluettelo</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          nimi: <input value={newName} onChange={updateNewName} />
+        </div>
+        <div>
+          <button type="submit">lisää</button>
+        </div>
+      </form>
+      <h2>Numerot</h2>
+      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+    </div>
+  )
+
+}
+
+export default App
