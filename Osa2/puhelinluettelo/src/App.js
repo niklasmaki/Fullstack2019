62be7a093ed72188bee8ae347dd4645328fc3b79
Niklas Mäki <niklas.maki@gmail.com>
2019-01-26 12:05:28 +0200

    2.8

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const updateNewName = (event) => {
     setNewName(event.target.value)
+  }
+
+  const updateNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -16,8 +24,9 @@ const App = () => {
       alert(`${newName} on jo luettelossa!`)
       return
     }
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({ name: newName, number: newNumber }))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -28,11 +37,18 @@ const App = () => {
           nimi: <input value={newName} onChange={updateNewName} />
         </div>
         <div>
+          numero: <input value={newNumber} onChange={updateNewNumber} />
+        </div>
+        <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+      {persons.map((person) => (
+        <div key={person.name}>
+          {person.name} {person.number}
+        </div>
+      ))}
     </div>
   )
 
