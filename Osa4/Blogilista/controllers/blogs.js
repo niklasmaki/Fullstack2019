ac894dca62dc7c23258adb3e8f333f7b24b98844
Niklas Mäki <niklas.maki@gmail.com>
2019-02-14 20:44:18 +0200

    4.17

--- Osa4/Blogilista/controllers/blogs.js
+++ Osa4/Blogilista/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -10,7 +12,14 @@ blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   try {
+    const user = await User.findOne({})
+    blog.user = user._id
+
     const result = await blog.save()
+
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+
     response.status(201).json(result)
   } catch (exception) {
     console.error(exception)
