4208d434003bdd79a72663d69f0480a04f5453df
Niklas Mäki <niklas.maki@gmail.com>
2019-02-14 11:59:37 +0200

    4.15

--- Osa4/Blogilista/app.js
+++ Osa4/Blogilista/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogRouter = require('./controllers/blogs')
 const config = require('./utils/config')
+const userRouter = require('./controllers/users')
 
 console.log('Connecting to MongoDB URL', config.mongoUrl)
 
@@ -15,5 +16,6 @@ mongoose.connect(config.mongoUrl, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', userRouter)
 
 module.exports = app
