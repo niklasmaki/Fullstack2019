3eea71c5a086f901814340252227534c60e1873c
Niklas Mäki <niklas.maki@gmail.com>
2019-02-16 16:40:50 +0200

    5.3

--- Osa5/bloglist-frontend/src/App.js
+++ Osa5/bloglist-frontend/src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
-import loginService from './services/login' 
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [user, setUser] = useState(null)
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [url, setUrl] = useState("")
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -19,6 +22,7 @@ const App = () => {
     const loggedInUser = window.localStorage.getItem('loggedInUser')
     if (loggedInUser) {
       const user = JSON.parse(loggedInUser)
+      blogService.setToken(user.token)
       setUser(user)
     }
   }, [])
@@ -31,6 +35,7 @@ const App = () => {
       })
 
       window.localStorage.setItem('loggedInUser', JSON.stringify(user))
+      blogService.setToken(user.token)
 
       setUser(user)
       setUsername('')
@@ -43,6 +48,19 @@ const App = () => {
   const handleLogout = async event => {
     window.localStorage.removeItem('loggedInUser')
     setUser(null)
+  }
+
+  const handleNewBlog = async event => {
+    event.preventDefault()
+    const newBlog = await blogService.create({
+      title,
+      author,
+      url
+    })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+    setBlogs(blogs.concat(newBlog))
   }
 
   if (user === null) {
@@ -89,6 +107,39 @@ const App = () => {
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+      <h2>Add a new blog</h2>
+      <form onSubmit={handleNewBlog}>
+        <div>
+          Title:
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          Author:
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          URL:
+          <input
+            type="text"
+            value={url}
+            name="URL"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button>
+          Add
+        </button>
+      </form>
     </div>
   )
 }
--- Osa5/bloglist-frontend/src/services/blogs.js
+++ Osa5/bloglist-frontend/src/services/blogs.js
@@ -2,9 +2,23 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+let token = null
+
+const setToken = newToken => {
+  token = `bearer ${newToken}`
 }
 
-export default { getAll }
+const getAll = async () => {
+  const result = await axios.get(baseUrl)
+  return result.data
+}
+
+const create = async blog => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const result = await axios.post(baseUrl, blog, config)
+  return result.data
+}
+
+export default { getAll, create, setToken }
