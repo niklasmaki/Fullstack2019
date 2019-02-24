c57b205cb45df2f73194c1b555ae0a36cdfdc88d
Niklas Mäki <niklas.maki@gmail.com>
2019-02-24 17:45:38 +0200

    5.14

--- Osa5/bloglist-frontend/src/components/simpleBlog.test.js
+++ Osa5/bloglist-frontend/src/components/simpleBlog.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import SimpleBlog from './SimpleBlog'
 
 test('renders content', async () => {
@@ -18,4 +18,24 @@ test('renders content', async () => {
   expect(component.container).toHaveTextContent('Best blog')
   expect(component.container).toHaveTextContent('Niklas M')
   expect(component.container).toHaveTextContent('blog has 14 likes')
+})
+
+test('handles likes properly', async () => {
+  const blog = {
+    title: 'Best blog',
+    author: 'Niklas M',
+    url: 'www.blog.com',
+    likes: 14
+  }
+
+  const handleLike = jest.fn()
+  const component = render(
+    <SimpleBlog blog={blog} onClick={handleLike} />
+  )
+
+  const button = component.getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(handleLike.mock.calls.length).toBe(2)
 })
