6d3963c4f85b5c4174c20395e4fb22774c67e194
Niklas Mäki <niklas.maki@gmail.com>
2023-02-23 21:07:25 +0200

    8.17

--- Osa8/library-frontend/src/queries.js
+++ Osa8/library-frontend/src/queries.js
@@ -13,7 +13,9 @@ export const ALL_AUTHORS = gql`
 export const ALL_BOOKS = gql`
   query AllAuthors {
     allBooks {
-      author
+      author {
+        name
+      }
       published
       title
       genres
